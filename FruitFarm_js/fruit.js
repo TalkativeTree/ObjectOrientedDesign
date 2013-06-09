7205740a33b0f0cf19b9b7f7e94ce6942c06e396
TalkativeTree <benjamin.b2@gmail.com>
2013-06-09 01:09:20 -0700

    js MVP done. still need to add fruit object interactions

--- FruitFarm_js/fruit.js
+++ FruitFarm_js/fruit.js
@@ -33,20 +33,18 @@ function FruitTree (){
 
 }
 
-// var FruitTree = {
-//   init: function(asd){
-//     rings: 0;
-//     this.status: "heatly"
-//     var fruit = [];
-//   }
-// }
 
-//-----------------Grove------------------//
-
+//-----------------Grove-------------------//
+/*
 function Grove (){
-
+  this.plants = []
 }
+*/
 
+Grove.prototype = new Farm();
+
+//-----------------Farm-------------------//
+//-----------------Farm-------------------//
 //-----------------Farm-------------------//
 
 function Farm (kind) {
@@ -54,42 +52,156 @@ function Farm (kind) {
   this.kind = kind
 }
 
+Farm.prototype.add = function(newCrop) { 
+  this.crops.push(newCrop); 
+}
+
+Farm.prototype.totalCrops = function(){
+//  console.log(this.crops.length);
+//  this.crops.length;
+  var total = this.crops.length;
+  return total;
+}
+
+Farm.prototype.grow = function(){
+//  console.log(this);
+//  for(var i = 0; i < this.totalCrops; i++){
+//  console.log("hi");
+for(var i = 0; i < this.totalCrops; i++){
+  this.crops[i].age();
+  };
+};
+
+Farm.prototype.yieldBounty = function(){
+  var bounty = [];
+  for(i=0;i < this.crops.length;i++){
+    bounty.push(this.crops[i].kind);
+  }
+//  console.log(bounty);
+  return bounty;
+  
+}
+//-----------------Farmer-----------------//
+//-----------------Farmer-----------------//
 //-----------------Farmer-----------------//
 
 function Farmer(name){
   this.name = name;
-  this.farms = [];
+  // this.farms = [];
 };
+
+
 
 Farmer.prototype.buyFarm = function(kind){
   var newFarm  = new Farm(kind);
-  this.farms.push(newFarm);
+  this.farm = (newFarm);
 };
 
-var ben = new Farmer("Ben")
+
+Farmer.prototype.plant = function(plant,kind,number){
+  for(var i = 0;i < number;i++){
+    var newCrop = plant;
+    newCrop.kind = kind;
+//  console.log(this.farm);
+    this.farm.add(newCrop);
+//  console.log(this.farm);
+  };
+};
+
+Farmer.prototype.countCrop = function(){
+//  console.log(this.farm.totalCrop());
+//  this.farm.totalCrop()
+  var total = this.farm.totalCrops();
+  return total
+}
+
+Farmer.prototype.waterCrops = function(){
+  ben.farm.grow();
+}
+
+
+
+Farmer.prototype.collectHarvest = function(){
+//  for(i=0;i<11;i++){
+//  console.log(ben.farm.crops[i].kind);
+//  ben.basket.push(ben.farm.yieldBounty());
+//  console.log(ben.farm.yieldBounty());
+//  };
+  var basket = ben.farm.yieldBounty();
+  return basket;
+}
+//
+//-----------------efficient prototyping-----------------//
+//-----------------efficient prototyping-----------------//
+Farmer.prototype = {
+  buyFarm: function(kind){
+    var newFarm  = new Farm(kind);
+    this.farm = (newFarm);
+  },
+  
+  plant: function(plant,kind,number){
+    
+    for(var i = 0;i < number;i++){
+      var newCrop = plant;
+      newCrop.kind = kind;
+      this.farm.add(newCrop);
+    };
+  },
+
+  countCrop: function(){
+    var total = this.farm.totalCrops();
+    return total
+  },
+
+  waterCrops: function(){
+    this.farm.grow();
+  },
+
+  collectHarvest: function(){
+    var basket = ben.farm.yieldBounty();
+    return basket;
+  }
+}
 
 
 //-----------------controllers-----------------//
 //$(function){
-  ben.buyFarm(new Grove);
-  console.log(ben.farms);
-  ben.plant("orange", 3);
-  console.log(bensFarm.totalCrop() === 3);
-  ben.plant("apple", 4);
-  console.log(bensFarm.totalCrop()  === 7);
-  ben.plant("pear", 4);
-  console.log(bensFarm.totalCrop() === 11);
-  for(var i = 0; i < 10; i++) {
-    ben.tendCrops();
-  }; //http://www.w3schools.com/js/js_loop_for.asp
-  ben.collectHarvest();
-  console.log(farmer.harvestSize() === farmer.totalCrops());
+var ben = new Farmer("Ben")
+ben.buyFarm("Grove");
+bensFarm = ben.farm;
+//  console.log(bensFarm.kind);
+ben.plant(new FruitTree, "orange", 3);
+//  console.log(bensFarm.crops);
+//  console.log(ben.farm.crops);
+//  bensNewFarm = ben.farm;
+//  console.log("total crop");
+console.log(ben.countCrop() === 3);
+ben.plant(new FruitTree, "apple", 4);
+console.log(ben.countCrop() === 7);
+ben.plant(new FruitTree, "pear", 4);
+console.log(ben.countCrop() === 11);
+//  for(var i = 0; i < 10; i++) {
+ben.waterCrops();
+//  };
+//console.log(ben);
+var harvest = {
+  bounty: ben.collectHarvest(),
+  bountyType: function(){
+    
+  },
+};
+console.log(harvest);
+//  };
 
-  ben.describeHarvestYield();
-  // "I harvested X oranges. I harvested X apples. I harvested X pears."
-  ben.describeFarm();
-  // "I have X orange trees, X pear trees, and X apple trees"
-// }
+
+
+//   console.log(farmer.harvestSize() === farmer.totalCrops());
+
+//   ben.describeHarvestYield();
+//   // "I harvested X oranges. I harvested X apples. I harvested X pears."
+//   ben.describeFarm();
+//   // "I have X orange trees, X pear trees, and X apple trees"
+// // }
 
 
 
