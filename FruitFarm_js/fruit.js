e8a02d4e16a1d4e326687cc592ba95dafdaacdff
TalkativeTree <benjamin.b2@gmail.com>
2013-06-08 18:04:06 -0700

    added driver code, defined variable ben.

--- FruitFarm_js/fruit.js
+++ FruitFarm_js/fruit.js
@@ -30,17 +30,57 @@ var stagesOfLife = {
 }
 
 function FruitTree (){
-  init: function(asd){
-    rings: 0;
-    asd: "fuck you work js"
-    this.fruit:  "sa";
-  };
+
 }
 
+// var FruitTree = {
+//   init: function(asd){
+//     rings: 0;
+//     this.status: "heatly"
+//     var fruit = [];
+//   }
+// }
+//-----------------Grove------------------//
 
-//-----------------Grove-----------------//
-
-//-----------------Controller-----------------//
 
 
-//-----------------View-----------------//
+//-----------------Farm-------------------//
+
+function Farm () {
+  this.crops = [],
+  this.
+}
+var bensFarm = new Farm()
+
+
+//-----------------Farmer-----------------//
+
+function Farmer(name){
+  this.name = name;
+}
+var ben = new Farmer("Ben")
+
+
+//-----------------controllers-----------------//
+//$(function){
+  ben.buyFarm(new Grove);
+  ben.plant("orange", 3);
+  console.log(bensFarm.totalCrop() === 3);
+  ben.plant("apple", 4);
+  console.log(bensFarm.totalCrop()  === 7);
+  ben.plant("pear", 4);
+  console.log(bensFarm.totalCrop() === 11);
+  for(var i = 0; i < 10; i++) {
+    ben.tendCrops();
+  }; //http://www.w3schools.com/js/js_loop_for.asp
+  ben.collectHarvest();
+  console.log(farmer.harvestSize() === farmer.totalCrops());
+
+  ben.describeHarvestYield();
+  // "I harvested X oranges. I harvested X apples. I harvested X pears."
+  ben.describeFarm();
+  // "I have X orange trees, X pear trees, and X apple trees"
+// }
+
+
+
