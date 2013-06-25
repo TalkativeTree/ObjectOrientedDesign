c314ce6e5bd28aec89ad15bcb718e9ff4af7d3f8
Ben Angel <benjamin.b2@gmail.com>
2013-06-25 14:38:50 -0600

    commented out Grove

--- FruitFarm_js/fruit.js
+++ FruitFarm_js/fruit.js
@@ -62,7 +62,7 @@ function Grove (){
 }
 */
 
-Grove.prototype = new Farm();
+//Grove.prototype = new Farm();
 
 //-----------------Farm-------------------//
 //-----------------Farm-------------------//
