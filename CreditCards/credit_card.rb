49c2fed5e43bd976981ea8ff1e603feab97ca712
Ben Angel <benjamin.b2@gmail.com>
2014-01-26 08:03:28 -0800

    removed InvalidCardNumber and changed CardNumber over to inherit from Struct. Also removed CardProcessor.valid? in favor of CardProcessor.find_errors to send errors along with CardNumber.

--- CreditCards/credit_card.rb
+++ CreditCards/credit_card.rb
@@ -1,42 +1,35 @@
-class CardNumber
-  attr_reader :number
-
-  def initialize(card_num)
-    @number = card_num
-  end
+class CardNumber < Struct.new(:number, :errors)
 end
 
-class InvalidCardNumber
-  attr_reader :number
-  def initialize(number)
-    @number = nil
-  end
-end
 
 module CardProcessor
   module_function
-
+  attr_accessor :errors
   def validate(number)
-    return CardNumber.new(number) if valid?(number)
-    InvalidCardNumber.new(number)
+    CardNumber.new(number, find_errors(number))
   end
 
-  def valid?(card_num)
+  def errors
+    @errors ||= []
+  end
+
+  def find_errors(card_num)
     right_size?(card_num) && right_format?(card_num) && passes_luhn_algorithm?(card_num)
+    errors
+  end
+
+  def right_size?(card_number)
+    errors << "Incorrect number of digits." if card_number.gsub(/-/,'').length != 16
+  end
+
+  def right_format?(card_number)
+    errors << "Invalid format." if card_number.gsub(/-/, '').match(/\D/)
   end
 
   def passes_luhn_algorithm?(card_number)
     numbers_between = card_number.gsub(/-/, '').split('').map{|num| num.to_i}
     digit_check = multiply_by_9 the_sum_of_all individual_digits_products_sum after_doubling_all_even numbers_between[0..14]
-    card_number[-1] == digit_check.to_s[-1]
-  end
-
-  def right_size?(card_number)
-    card_number.gsub(/-/,'').length == 16
-  end
-
-  def right_format?(card_number)
-    !card_number.gsub(/-/, '').match(/\D/)
+    errors << "Invalid number." if card_number[-1] == digit_check.to_s[-1]
   end
 
   def after_doubling_all_even(numbers)
@@ -60,13 +53,15 @@ end
 
 class CreditCard
   include CardProcessor
-  attr_reader :number
+  attr_reader :number, :errors
 
   def initialize(number)
-    @number = CardProcessor.validate(number).number
+    processed = CardProcessor.validate(number)
+    @number = processed.number
+    @errors = processed.errors
   end
 
   def valid?
-    !number.nil?
+    @errors.any?
   end
 end
