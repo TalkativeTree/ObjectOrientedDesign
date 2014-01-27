653553e498d2ea16cf1258adaa5e773a20ad9384
Ben Angel <benjamin.b2@gmail.com>
2014-01-26 16:09:35 -0800

    redid error concatination

--- CreditCards/credit_card.rb
+++ CreditCards/credit_card.rb
@@ -4,32 +4,29 @@ end
 
 module CardProcessor
   module_function
-  attr_accessor :errors
+
   def validate(number)
     CardNumber.new(number, find_errors(number))
   end
 
-  def errors
-    @errors ||= []
-  end
-
   def find_errors(card_num)
-    right_size?(card_num) && right_format?(card_num) && passes_luhn_algorithm?(card_num)
-    errors
+    errors = []
+    errors << check_size(card_num) << check_format(card_num) << check_luhn_algorithm(card_num)
+    errors.compact
   end
 
-  def right_size?(card_number)
-    errors << "Incorrect number of digits." if card_number.gsub(/-/,'').length != 16
+  def check_size(card_number)
+    "Incorrect number of digits." if card_number.gsub(/-/,'').length != 16
   end
 
-  def right_format?(card_number)
-    errors << "Invalid format." if card_number.gsub(/-/, '').match(/\D/)
+  def check_format(card_number)
+    "Invalid format." if card_number.gsub(/-/, '').match(/\D/)
   end
 
-  def passes_luhn_algorithm?(card_number)
+  def check_luhn_algorithm(card_number)
     numbers_between = card_number.gsub(/-/, '').split('').map{|num| num.to_i}
     digit_check = multiply_by_9 the_sum_of_all individual_digits_products_sum after_doubling_all_even numbers_between[0..14]
-    errors << "Invalid number." if card_number[-1] == digit_check.to_s[-1]
+    "Invalid number." if card_number[-1] != digit_check.to_s[-1]
   end
 
   def after_doubling_all_even(numbers)
